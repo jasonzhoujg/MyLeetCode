e3a22e0ebb21fb534e22c38d772a3f020f17971d
jzhoujg <774780180@qq.com>
2021-09-02 17:11:23 +0800

    0902-pm

--- codes/28/28.py
+++ codes/28/28.py
@@ -1,32 +1,40 @@
-class Solution:
-    def strStr(self, haystack: str, needle: str) -> int:
-        if not needle:
-            return 0
-        if not haystack:
-            return -1
 
-        next_a = [0]
-        now = 0
-        for i in range(1,len(needle)):
-            if needle[i]==needle[now]:
-                next_a.append(now+1)
-            elif now:
-                now = next_a[now-1]
-                i -= 1
-            else: 
-                next_a.append(0)
-
-        k = 0
-        N = len(needle)
-        while k+N<=len(haystack):
-            for j in range(N):
-                if k+j==len(haystack):
-                    return -1
-                if not haystack[k+j]==needle[j]:
-                    k+=next_a[j]
-                    break
-                else:
-                    if j == N-1:
-                        return k
-            k += 1
+def strStr(haystack: str, needle: str) -> int:
+    if not needle:
+        return 0
+    if not haystack:
         return -1
+
+    next_a = [0]
+    now = 0
+
+    for i in range(1, len(needle)):
+        if needle[i] == needle[now]:
+            now += 1
+            next_a.append(now)
+        elif now:
+            now = next_a[now - 1]
+            i -= 1
+        else:
+            next_a.append(0)
+    k = 0
+    p = 0
+    N = len(needle)
+    M = len(haystack)
+    # ini = 0
+    while p <= M - 1:
+        if needle[k] == haystack[p] and k < N - 1:
+            k += 1
+            p += 1
+        elif needle[k] == haystack[p] and k == N - 1:
+            return p - N + 1
+        else:
+            p += next_a[k] + 1 - k
+            k = 0
+
+
+    return -1
+
+
+print(strStr(haystack="mississippi",
+             needle="issip"))
